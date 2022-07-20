f6824bd485a911723865688420705f0786c1948c
UlianaOr <Ouliana9@gmail.com>
2022-07-20 10:42:38 +0200

    test2

--- src/01.test.tsx
+++ src/01.test.tsx
@@ -1,4 +1,4 @@
-import { sum } from "./01";
+import { mult, sum } from "./01";
 
 test ('sum should be correct', () => {
     //data
@@ -13,6 +13,23 @@ test ('sum should be correct', () => {
     // expect result
     expect (result1).toBe (3);
     expect (result2). toBe (5);
+
+
+})
+
+test ('multiply should be correct', () => {
+    //data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = mult (a,b);
+    const result2 = mult (b,c);
+
+    // expect result
+    expect (result1).toBe (2);
+    expect (result2). toBe (6);
 
 
 })
--- src/01.tsx
+++ src/01.tsx
@@ -1,7 +1,7 @@
 export const value01_01 = "01_01";
 
 export function sum (a: number, b: number) {
-    return a +b;
+    return a + b;
 
 }
 

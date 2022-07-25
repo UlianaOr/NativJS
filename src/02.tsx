a1855471b3e8ebbb4b11dfc8c3d553f36d5ea5c1
UlianaOr <Ouliana9@gmail.com>
2022-07-25 15:44:32 +0200

    object1

--- src/02.tsx
+++ src/02.tsx
@@ -1,12 +1,36 @@
 const student = {
+    id: 1,
     "name": "Uliana",
      age: 36,
-     isActiv: false
+     isActiv: false,
+     addresss: {
+        streetTitle: "PerisCelda 15",
+        city: {
+            title: "Valencia",
+            countryTitle: "España"
+        }
+     },
+     technologies: [
+        {
+            id: 1,
+            title: "HTML5"
+        },
+        {
+            id: 2,
+            title : "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+     ]
 
 }
 
 console.log (student.age)
 console.log (student.name)
+console.log (student.addresss.city.title)
+console.log (student.technologies[2].title)
 
 /*const scool = {
     name: "IT-incubator",
